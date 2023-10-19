d0a8b90dd45c74fd192c01a941a1fcba05b8d75b
hfzhart <zmykalo.denys@student.uzhnu.edu.ua>
2023-10-20 01:47:20 +0300

    first changes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+function Header() {
+  return (
+    <div className="container">
+      <p>Hello</p>
+      <p>Niggers</p>
+      useState
+          </div>
+  );    
+}
+
+export default Header;
+
