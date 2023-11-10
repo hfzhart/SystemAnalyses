a40b9765dfa348ff9dc37ae47eaf79c6b510f4e1
hfzhart <zmykalo.denys@student.uzhnu.edu.ua>
2023-11-10 23:42:33 +0200

    test

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer/foter";
+import './Home.css'
+function Home() {
+  return (
+    <div className="home-container">
+      <Header />
+      <div className="TextContainer">
+      <h1>Welcome!</h1>
+      <h1>Login is success!</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
