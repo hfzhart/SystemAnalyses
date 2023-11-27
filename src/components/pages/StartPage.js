8432c82504fbf01e736bf7932cad79291c759f5c
hfzhart <zmykalo.denys@student.uzhnu.edu.ua>
2023-11-28 00:10:30 +0200

    adding some content to start page, adding side bar user profile

--- src/components/pages/StartPage.js
+++ src/components/pages/StartPage.js
@@ -2,52 +2,60 @@ import React from "react";
 import MyLineChart from "../Lines/MyLineChart";
 import Header from "../Header";
 import Footer from "../Footer/foter";
-import './StartPage.css'
+import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './StartPage.css';
 
 function StartPage() {
   return (
     <div className="start-page-container">
       <Header />
-     
-      <div className="content-container">
-        <h1>Welcome to start page!
-          Тест Тест</h1>     
-      </div>
-      {/* <div className="LineChart">
-        <MyLineChart/>   
-      </div> */}
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
-      <div className="">
-        <MyLineChart/>   
-      </div>
+      <div className="spacer2"></div>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          backgroundColor: '#F1F4F9',
+          color: 'black',
+          padding: '10px', 
+          minHeight: '150px', 
+        }}
+      >
+        <h1 style={{ margin: 0, fontSize: '1.5rem' }}>Unlock the power of your products</h1>
+        <Box sx={{ width: '100%', marginTop: '5px' }}>
+          <Button variant="contained" color="primary" component={Link} to="/register" sx={{ margin: 'auto' }}>
+            Реєстрація
+          </Button>
+          <h1 style={{ maxWidth: '600px', margin: '10px auto', fontSize: '1rem' }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </h1>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          opacity: 0,
+          transform: 'translateY(100px)',
+          transition: 'opacity 0.5s, transform 0.5s',
+        }}
+        sx1={{
+          marginTop: '20px',
+        }}
+      >
+        <MyLineChart />
+      </Box>
 
       <div className="spacer"></div>
+
       <div className="footer-wrapper">
         <Footer />
       </div>
--- src/components/pages/UserProfile/UserProfile.js
+++ src/components/pages/UserProfile/UserProfile.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Avatar, Button, Menu, MenuItem,Grid,CardContent,CardActions,Card,Box} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import {Typography,Avatar,Menu,MenuItem,Grid,CardContent,CardActions,Box, Drawer, List, ListItem, ListItemText, CssBaseline, IconButton,} from '@mui/material';
+import { useNavigate, Link, Outlet } from 'react-router-dom';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Divider from '@mui/material/Divider';
+import SaveIcon from '@mui/icons-material/Save';
 import avatar1 from '../../../uploads/Avatar1.png';
 import avatar2 from '../../../uploads/Avatar2.png';
 import avatar3 from '../../../uploads/Avatar3.png';
@@ -12,7 +20,6 @@ import './UserProfile.css';
 
 const UserProfile = () => {
   const navigate = useNavigate();
-  const isLoggedIn = localStorage.getItem('token') !== null;
   const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6];
   const [user, setUser] = useState({
     id: null,
@@ -21,15 +28,20 @@ const UserProfile = () => {
     avatarUrl: '',
   });
   const [anchorEl, setAnchorEl] = useState(null);
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+  const [previewAvatar, setPreviewAvatar] = useState(user.avatarUrl || null);
 
- const fetchUserData = async () => {
+  const fetchUserData = async () => {
     try {
       const storedUser = localStorage.getItem('user');
       const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-
+  
       if (storedUser && isLoggedIn) {
         const user = JSON.parse(storedUser);
-        setUser(user);
+        const response = await axios.get(`http://localhost:3001/users/${user.id}`);
+        const updatedUser = response.data;
+        setUser(updatedUser);
       } else {
         setUser({
           id: null,
@@ -45,8 +57,13 @@ const UserProfile = () => {
 
   useEffect(() => {
     fetchUserData();
-  }, [isLoggedIn]);
-
+  }, []);
+  
+  useEffect(() => {
+    console.log('user.avatarUrl changed:', user.avatarUrl);
+    setPreviewAvatar(user.avatarUrl || null);
+  }, [user.avatarUrl]);
+  
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -56,7 +73,8 @@ const UserProfile = () => {
   };
 
   const handleAvatarChange = (newAvatarUrl) => {
-    setUser((prevUser) => ({ ...prevUser, avatarUrl: newAvatarUrl }));
+    setSelectedAvatar(newAvatarUrl);
+    setPreviewAvatar(newAvatarUrl);
     handleMenuClose();
   };
 
@@ -69,7 +87,7 @@ const UserProfile = () => {
         email: '',
         avatarUrl: '',
       });
-
+      setPreviewAvatar(null);
       localStorage.removeItem('user');
       localStorage.setItem('isLoggedIn', 'false');
       navigate('/');
@@ -78,40 +96,122 @@ const UserProfile = () => {
     }
   };
 
+  const handleSaveAvatar = async () => {
+    try {
+      await axios.patch(`http://localhost:3001/users/${user.id}`, { avatarUrl: selectedAvatar });
+      setUser((prevUser) => ({ ...prevUser, avatarUrl: selectedAvatar }));
+      setPreviewAvatar(selectedAvatar);
+      toast.success('Аватарка успішно збережена!');
+    } catch (error) {
+      console.error('Error saving avatar:', error);
+      toast.error('Помилка при зберіганні аватарки.');
+    }
+  };
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+
+
   return (
-    <div className='ProfileCard'>
-    <Box display="flex" className="Box">
-      <Card style={{ width: '300px', margin: '20px' }}>
-        <CardContent>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="h6">{user.username}</Typography>
-              <Typography variant="body2">{user.email}</Typography>
+    <div className={`ProfileCard ${isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
+      <CssBaseline />
+
+      <Box display="flex">
+
+       
+        <Drawer
+          variant="persistent"
+          anchor="left"
+          open={isSidebarOpen}
+          sx={{ 
+            zIndex: 1200,
+            '& .MuiPaper-root': {
+              backgroundColor: '#F1F4F9', 
+            },
+          }}
+        >
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="h6">{user.username}</Typography>
+                <Typography variant="body2">{user.email}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Avatar alt="User Avatar" src={previewAvatar} sx={{ width: 150, height: 150, margin: '15px' }} />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Avatar alt="User Avatar" src={user.avatarUrl} sx={{ width: 100, height: 100, margin: '10px' }} />
-            </Grid>
-          </Grid>
-        </CardContent>
-        <CardActions>
-          <Button variant="outlined" onClick={handleMenuOpen}>
-            Змінити аватарку
-          </Button>
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} PaperProps={{ style: { width: '200px' } }}>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-              {avatars.map((avatar, index) => (
-                <MenuItem key={index} onClick={() => handleAvatarChange(avatar)}>
-                  <Avatar alt={`Аватар ${index + 1}`} src={avatar} sx={{ width: '30px', height: '30px', marginRight: '5px' }} />
-                </MenuItem>
-              ))}
-            </div>
-          </Menu>
-          <Button variant="outlined" onClick={handleLogout}>
-            Вийти
-          </Button>
-        </CardActions>
-      </Card>
-    </Box>
+          </CardContent>
+          <IconButton
+            color="inherit"
+            aria-label="close sidebar"
+            onClick={toggleSidebar}
+            sx={{
+              position: 'absolute',
+              top: 10,
+              right: 10,
+              fontSize: 30,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <CardActions>
+            <IconButton onClick={handleMenuOpen}>
+              <SettingsIcon />
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} PaperProps={{ style: { width: '450px' } }}>
+              <div style={{ display: 'flex', flexWrap: 'wrap',padding: '16px' }}>
+                {avatars.map((avatar, index) => (
+                  <MenuItem key={index} onClick={() => handleAvatarChange(avatar)}>
+                    <Avatar alt={`Аватар ${index + 1}`} src={avatar} sx={{ width: '100px', height: '100px', marginRight: '5px' }} />
+                  </MenuItem>
+                ))}
+              </div>
+            </Menu>
+            <IconButton onClick={handleLogout}>
+              <ExitToAppIcon/>
+            </IconButton>
+            <IconButton onClick={handleSaveAvatar}>
+              <SaveIcon />
+            </IconButton>
+          </CardActions>
+          <List>
+            {['Dashboard 1', 'Dashboard 2', 'Dashboard 3'].map((text, index) => (
+              <div key={text}>
+                <ListItem button component={Link} to={`/dashboard/${index + 1}`} onClick={toggleSidebar}>
+                    <DashboardIcon />
+                  <ListItemText primary={text} />
+                </ListItem>
+                {index !== 2 && <Divider />} 
+              </div>
+            ))}
+          </List>
+          <Divider />
+        </Drawer>
+
+        
+        <IconButton
+          color="inherit"
+          aria-label="open sidebar"
+          onClick={toggleSidebar}
+          sx={{
+            fontSize: 40,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+       
+        <Box ml={isSidebarOpen ? 300 : 0} p={2}>
+          <Outlet />
+        </Box>
+
+      </Box>
+
+      
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+
     </div>
   );
 };
